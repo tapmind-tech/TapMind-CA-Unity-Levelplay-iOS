9118cabf78303cff7529ffb88fbb7ff91bf94983
Kishan Italiya <kishan.italiya@tuvoc.com>
2026-06-23 16:13:19 +0530

    Removed logs

--- publish_pod.sh
+++ publish_pod.sh
@@ -19,7 +19,7 @@ fi
 echo "🏷️  Tagging version $VERSION in git..."
 git add .
 #git commit -m "Release version $VERSION"
-git commit -m "Resolved the crash issue for Unity"
+git commit -m "Removed logs"
 git tag "$VERSION"
 git push origin main
 git push origin "$VERSION"
